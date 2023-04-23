3784faae949e128508f664c80279e2276d73de1a
Tirok99 <98711612+Tirok99@users.noreply.github.com>
2023-04-23 20:18:51 +0300

    Add files via upload

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -2,8 +2,19 @@
 document.addEventListener("click", function (e) {
 	const targetElement = e.target;
 
-	if (targetElement.closest('.menu-icon')) {
-		document.documentElement.classList.toggle('menu-open');
+	if (targetElement.closest('.menu-block-icon')) {
+		document.documentElement.classList.toggle('menu-block-open');
+		e.preventDefault();
+	}
+
+})
+
+
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+
+	if (targetElement.closest('.header__icon-menu')) {
+		document.documentElement.classList.toggle('header-open');
 		e.preventDefault();
 	}
 
